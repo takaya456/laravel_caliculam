8d7437fe7c429e63643cfb15e4a9787c478018bd
takaya456 <5milingmarineday@gmail.com>
2021-08-21 18:33:12 +0900

    コントローラクラスやテーブルの操作用モデルクラスの作成, ルーティングなどを行った

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Post;
+
+class PostController extends Controller
+{
+	/*
+	 * Post一覧を表示する
+	 *
+	 * @param Post Postモデル
+	 * @return array Postモデルリスト
+	 */
+	public function index(Post $post)
+	{
+		return $post->get();
+	}
+}
