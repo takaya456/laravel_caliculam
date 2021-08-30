9920da7a321a7143a3395ab1eae3246967f4e3cb
takaya456 <5milingmarineday@gmail.com>
2021-08-30 10:07:21 +0900

    記事の編集画面を作成した

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -39,4 +39,16 @@ class PostController extends Controller
 		$post->fill($input)->save();
 		return redirect('/posts/'. $post->id);
 	}
+	
+	public function edit(Post $post)
+	{
+		return view('edit')->with(['post' => $post]);
+	}
+	
+	public function update(Post $post, PostRequest $request)
+	{
+		$input = $request['post'];
+		$post->fill($input)->save();
+		return redirect('/posts/'. $post->id);
+	}
 }
